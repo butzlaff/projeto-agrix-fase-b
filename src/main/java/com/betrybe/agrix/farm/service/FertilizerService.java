3fe41989b742dc2bc0e35c9d560fb4d14d68a4bb
butzlaff <emiliobutz@gmail.com>
2023-12-03 20:35:20 -0300

    feat: FindbyId and FindAll functions on service for fertiliziers

--- src/main/java/com/betrybe/agrix/farm/service/FertilizerService.java
+++ src/main/java/com/betrybe/agrix/farm/service/FertilizerService.java
@@ -1,9 +1,13 @@
 package com.betrybe.agrix.farm.service;
 
-import com.betrybe.agrix.farm.entity.Fertilizer;
-import com.betrybe.agrix.farm.repository.FertilizerRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.betrybe.agrix.farm.entity.Fertilizer;
+import com.betrybe.agrix.farm.repository.FertilizerRepository;
+import com.betrybe.agrix.farm.service.exceptions.FertilizerNotFoundException;
 
 /**
  * Service for Fertilizar.
@@ -22,5 +26,17 @@ public class FertilizerService {
    */
   public Fertilizer saveFertilizer(Fertilizer fertilizer) {
     return fertilizerRepository.save(fertilizer);
+  }
+  /**
+   * Find a fertilizer by id.
+   */
+  public Fertilizer findById(Long id) throws FertilizerNotFoundException {
+    return fertilizerRepository.findById(id).orElseThrow(FertilizerNotFoundException::new);
+  }
+  /**
+   * Find and returns a list of all Fertiliziers.
+   */
+  public List<Fertilizer> findAllFertilizers() {
+    return fertilizerRepository.findAll();
   }
 }
